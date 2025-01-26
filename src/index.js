358f3e8afc10a3baa55c39f0d5f7f72f4d1ac8ac
João Guilherme Fernandes Borba <168598901+JoaoGuilherme527@users.noreply.github.com>
2025-01-26 15:07:10 -0300

    add description

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import reportWebVitals from "./reportWebVitals"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <React.StrictMode>
-            <head>
-                <meta name="viewport" content="width=device-width, user-scalable=no" />
-            </head>
-            <App />
+        <App />
     </React.StrictMode>
 )
 
